c251bc3790129ae522ff3c9450589fc01caa29bc
BuschMath <busch@iowacentral.edu>
2024-10-23 09:53:07 -0500

    For loop

--- Ch7_Example_FA24/Main.cpp
+++ Ch7_Example_FA24/Main.cpp
@@ -2,55 +2,28 @@
 
 using namespace std;
 
-void AddCustomer();
-void UpdateAccount();
-
 int main()
 {
-	char menu_input = '0';
-	bool loop;// = true;
+	int number = 7;
+	int numberCopy = number;
+	int power = 5;
 
-	do
+	for (int i = 2; i <= power; i++)
 	{
-		cout << "Customer information program.\nMenu options:\n";
-		cout << "Enter 1 to add customer.\nEnter 2 to update account\nEnter 3 to exit.\n";
+		number = number * numberCopy;
+		cout << number << endl;
+	}
 
-		cin >> menu_input;
+	cout << number << endl << endl;
 
-		if (!cin)
+	for (int i = 1; i <= 10; i = i + 2)
+	{
+		cout << i << endl;
+		for (int j = 1; j <= 5; j++)
 		{
-			cout << "Stream error, exiting program.\n";
-			break;
+			cout << "\t" << j << endl;
 		}
-
-		switch (menu_input)
-		{
-		case '1':	AddCustomer();
-			loop = true;
-			break;
-
-		case '2':	UpdateAccount();
-			loop = true;
-			break;
-
-		case '3':	cout << "Exiting program.\n\n";
-			loop = false;
-			break;
-
-		default:	cout << "Input Error, try again\n\n";
-			break;
-		}
-	} while (loop);
+	}
 
 	return 0;
-}
-
-void AddCustomer()
-{
-	cout << "Customer added!\n\n";
-}
-
-void UpdateAccount()
-{
-	cout << "Account updated!\n\n";
 }
