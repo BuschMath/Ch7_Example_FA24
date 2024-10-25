817e5f3dc313a8acb4c2ba7d8ecfc9b4b3b94469
BuschMath <busch@iowacentral.edu>
2024-10-25 09:47:09 -0500

    Break, continue

--- Ch7_Example_FA24/Main.cpp
+++ Ch7_Example_FA24/Main.cpp
@@ -4,23 +4,16 @@ using namespace std;
 
 int main()
 {
-	int number = 7;
-	int numberCopy = number;
-	int power = 5;
-
-	for (int i = 2; i <= power; i++)
-	{
-		number = number * numberCopy;
-		cout << number << endl;
-	}
-
-	cout << number << endl << endl;
-
-	for (int i = 1; i <= 10; i = i + 2)
+	for (int i = 1; i <= 10; i++)
 	{
 		cout << i << endl;
-		for (int j = 1; j <= 5; j++)
+		for (int j = 1; j <= 10; j++)
 		{
+			if (i % 3 == 0)
+				continue;
+			if (j == i)
+				break;
+
 			cout << "\t" << j << endl;
 		}
 	}
